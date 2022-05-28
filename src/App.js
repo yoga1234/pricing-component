7f4af5fe54918692ec813e008b609d2508e6d0e0
Septiana Yoga <septianayoga30@gmail.com>
2022-05-28 14:27:31 +0700

    adding header and basic slider

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Our Pricing</h1>
-      <p>Toggle will goes here</p>
+      <h1 className="main-title">Our Pricing</h1>
+      <label className="switch">
+        <input type="checkbox" />
+        <span className="slider round"></span>
+      </label>
       <p>component goes here</p>
     </div>
   );
